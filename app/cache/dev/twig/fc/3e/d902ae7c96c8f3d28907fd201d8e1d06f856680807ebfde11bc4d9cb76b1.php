f6887f7591e76a5bcbfa13ef39744193f4b12c0a
mimadl3a <imad.karaoui@gmail.com>
2014-05-16 23:28:11 +0200

    Installation

---
+++ app/cache/dev/twig/fc/3e/d902ae7c96c8f3d28907fd201d8e1d06f856680807ebfde11bc4d9cb76b1.php
@@ -0,0 +1,65 @@
+<?php
+
+/* CrudTestBundle:Produit:new.html.twig */
+class __TwigTemplate_fc3ed902ae7c96c8f3d28907fd201d8e1d06f856680807ebfde11bc4d9cb76b1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "<h1>Produit creation</h1>
+
+    ";
+        // line 6
+        echo         $this->env->getExtension('form')->renderer->renderBlock((isset($context["form"]) ? $context["form"] : $this->getContext($context, "form")), 'form');
+        echo "
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 10
+        echo $this->env->getExtension('routing')->getPath("produit");
+        echo "\">
+            Back to the list
+        </a>
+    </li>
+</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CrudTestBundle:Produit:new.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  42 => 10,  35 => 6,  31 => 4,  28 => 3,);
+    }
+}

5210afc7c10cd042c98a8229ae68716f6868ae88
mimadl3a <imad.karaoui@gmail.com>
2014-05-18 17:35:34 +0200

    modification du fichier gitignore

---
+++ app/cache/dev/twig/87/a6/2b0e9a9f297cdce2e71d3cf9cf3d11bd0e16bafa14c598afe5fbaafee40b.php
@@ -0,0 +1,98 @@
+<?php
+
+/* CrudTestBundle:Produit:show.html.twig */
+class __TwigTemplate_87a62b0e9a9f297cdce2e71d3cf9cf3d11bd0e16bafa14c598afe5fbaafee40b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "<h1>Produit</h1>
+
+    <table class=\"record_properties\">
+        <tbody>
+            <tr>
+                <th>Id</th>
+                <td>";
+        // line 10
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Libelle</th>
+                <td>";
+        // line 14
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "libelle"), "html", null, true);
+        echo "</td>
+            </tr>
+            <tr>
+                <th>Prix</th>
+                <td>";
+        // line 18
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "prix"), "html", null, true);
+        echo "</td>
+            </tr>
+        </tbody>
+    </table>
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 25
+        echo $this->env->getExtension('routing')->getPath("produit");
+        echo "\">
+            Back to the list
+        </a>
+    </li>
+    <li>
+        <a href=\"";
+        // line 30
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("produit_edit", array("id" => $this->getAttribute((isset($context["entity"]) ? $context["entity"] : $this->getContext($context, "entity")), "id"))), "html", null, true);
+        echo "\">
+            Edit
+        </a>
+    </li>
+    <li>";
+        // line 34
+        echo         $this->env->getExtension('form')->renderer->renderBlock((isset($context["delete_form"]) ? $context["delete_form"] : $this->getContext($context, "delete_form")), 'form');
+        echo "</li>
+</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CrudTestBundle:Produit:show.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  78 => 34,  71 => 30,  63 => 25,  53 => 18,  46 => 14,  39 => 10,  31 => 4,  28 => 3,);
+    }
+}
